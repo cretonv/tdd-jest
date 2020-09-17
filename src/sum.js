795b09349394eff2f8f4a9db7594e3408e907eb8
Vincent Creton <vincecreton@hotmail.fr>
2020-09-17 09:56:25 +0200

    FEATURE: Ajout des 4 premières tâches et test de Travis CI

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,10 @@
 // Implement the sum function here
+
+function sum(numb1, numb2) {
+    if( typeof numb1 === "number" && typeof numb2 === "number") {
+        return numb1 + numb2
+    }
+    return false
+}
+
+module.exports = sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,15 @@
 // Add the unit tests of the sum function here
+import sum from "../src/sum.js"
+
+describe('Verify sum function', () => {
+
+    test ('Les 2 arguments passés en paramètres sont pas des nombres', () => {
+        expect(sum('1', 2)).toBe(false)
+    })
+
+    test ('additionné 1 + 2 pour obtenir 3', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+
+
+})
